97a77182f6b8b73aa140bfbee7ac53d42b5a4e14
Chris Jaure <chrisjaure@gmail.com>
2008-07-10 15:26:49 +0000

    Indonesian translation!

---
+++ source/localization/id.php
@@ -0,0 +1,186 @@
+<?php
+// +-------------------------------------------------------------------------------+
+// | id.php                                                                        |
+// +-------------------------------------------------------------------------------+
+// | Copyright (c) 2008 Irwan Butar Butar, All rights reserved.                    |
+// | Version       0.1                                                             |
+// | Last modified 2008-07-09                                                      |
+// | Email         irwansah.putra@gmail.com                                        |
+// | Web           http://www.kupluk.com                                           |
+// +-------------------------------------------------------------------------------+
+// |                                                                               |
+// | The MIT License                                                               |
+// |                                                                               |
+// | Copyright 2008 Chris Jaure, <http://www.jibberbook.com/>                      |
+// |                                                                               |
+// | Permission is hereby granted, free of charge, to any person obtaining a copy  |
+// | of this software and associated documentation files (the "Software"), to deal |
+// | in the Software without restriction, including without limitation the rights  |
+// | to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     |
+// | copies of the Software, and to permit persons to whom the Software is         |
+// | furnished to do so, subject to the following conditions:                      |
+// |                                                                               |
+// | The above copyright notice and this permission notice shall be included in    |
+// | all copies or substantial portions of the Software.                           |
+// |                                                                               |
+// | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    |
+// | IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      |
+// | FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   |
+// | AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        |
+// | LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, |
+// | OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     |
+// | THE SOFTWARE.                                                                 |
+// |                                                                               |
+// |-------------------------------------------------------------------------------+
+// |                                                                               | 
+// | If you use this software commercially, please consider donating               |
+// | via PayPal to ckeuler@gmail.com                                               |
+// |                                                                               |
+// +-------------------------------------------------------------------------------+
+
+/**
+ * Jibberbook Indonesian (Bahasa) translation. 
+ * Based on jibberbook version 2.1.2
+ *
+ * Jibbberbook - An Ajax Guestbook is 
+ * Copyright 2008 Chris Jaure, <http://www.jibberbook.com/>
+ *  
+ * @version   0.1 (2008-07-09)
+ * @author    Irwan Butar Butar (irwansah.putra@gmail.com)
+ * @license   The MIT License
+ * @copyright 2008 Irwan Butar Butar, All rights reserved.  
+ */
+$lang = array(
+    "An error has occurred." =>
+    "Telah terjadi kesalahan.",
+    
+    "All comments have been loaded." =>
+    "Semua komentar sudah dimuat.",
+    
+    "Loading..." =>
+    "Sedang memuat...",
+    
+    "Your comment could not be added. Please try again later." =>
+    "Komentar Anda tidak berhasil dimuat. Mohon coba beberapa saat lagi.",
+    
+    "More comments are loading. If you are using the scrollbar, please release your mouse." =>
+    "Lanjutan daftar komentar sedang dimuat. Jika Anda menggunakan scrollbar, mohon lepas tekanan pada mouse Anda.",
+    
+    "Your comment has been added." =>
+    "Komentar Anda telah ditambahkan.",
+    
+    "writes..." =>
+    "menulis...",
+    
+    "Posted on" =>
+    "Ditulis pada",
+    
+    "Website is not a valid URL." =>
+    "URL situs web yang Anda masukkan tidak benar.",
+    
+    "Name and Comment fields are required." =>
+    "Anda belum mengisi Nama dan Komentar.",
+    
+    "This comment has been flagged as spam and has been added to the moderation queue." =>
+    "Komentar ini telah ditandai sebagai spam dan telah ditambahkan pada daftar antrian yang akan diperiksa.",
+    
+    "Comment" =>
+    "Kommentar",
+    
+    "Name" =>
+    "Nama",
+    
+    "Website" =>
+    "Situs Web",
+    
+    "required" =>
+    "wajib diisi",
+    
+    "Add Comment" =>
+    "Tambah Komentar",
+    
+    "Do not fill out this field." =>
+    "Kotak ini jangan diisi.",
+    
+    "Only the most recent comments are showing. You need JavaScript to view them all." =>
+    "Hanya komentar-komentar terbaru yang ditampilkan. Anda membutuhkan Javascript untuk menampilkan semuanya.",
+    
+    "Add it!" =>
+    "Simpan!",
+    
+    "Comments" =>
+    "Daftar Komentar",
+    
+    "Log In" =>
+    "Log In",
+    
+    "Password" =>
+    "Kata Sandi",
+    
+    "All Ham" =>
+    "Semua Komentar",
+    
+    "All Spam" =>
+    "Semua Spam",
+    
+    "Recent Ham" =>
+    "Komentar Terbaru",
+    
+    "Recent Spam" =>
+    "Spam Terbaru",
+    
+    "Delete All Spam" =>
+    "Hapus Semua Spam",
+    
+    "Spam" =>
+    "Spam",
+    
+    "Ham" =>
+    "Komentar",
+    
+    "Recent" =>
+    "Terbaru",
+    
+    "Logout" =>
+    "Keluar",
+    
+    "View Guestbook" =>
+    "Lihat Daftar Komentar",
+    
+    "Dashboard" =>
+    "Dashboard",
+    
+    "The comment has been deleted." =>
+    "Komentar telah berhasil dihapus.",
+    
+    "All spam comments have been deleted." =>
+    "Semua komentar spam telah berhasil dihapus.",
+    
+    "No comments to load." =>
+    "Tidak ada komentar untuk dimuat.",
+    
+    "Incorrect password." =>
+    "Kata sandi tidak sesuai.",
+    
+    "The comment has been reclassified." =>
+    "Komentar telah diklasifikasi ulang.",
+    
+    "Not Spam" =>
+    "Bukan Spam",
+    
+    "Delete" =>
+    "Hapus",
+    
+    "First" =>
+    "Pertama",
+    
+    "Previous" =>
+    "Sebelum",
+    
+    "Next" =>
+    "Berikut",
+    
+    "Last" =>
+    "Terakhir"
+);
+?>
