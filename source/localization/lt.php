9ae48a33cc9c24036d0747a42e3ffb9a55fe6c81
Chris Jaure <chrisjaure@gmail.com>
2008-10-01 20:08:17 +0000

    Added two new language packs and correct language code.

---
+++ source/localization/lt.php
@@ -0,0 +1,137 @@
+<?php
+// lietuvių kalba
+
+$lang = array(
+    "An error has occurred." =>
+    "Klaida.",
+    
+    "All comments have been loaded." =>
+    "Visi komentarai užkrauti",
+    
+    "Loading..." =>
+    "Kraunama..",
+    
+    "Your comment could not be added. Please try again later." =>
+    "Jūsų komentaras nepridėtas. Prašome pabandyti vėliau.",
+    
+    "More comments are loading. If you are using the scrollbar, please release your mouse." =>
+    "Daugiau komentarų kraunama. Jei naudojate slankties juosta, prašome ją atleisti.",
+    
+    "Your comment has been added." =>
+    "Jūsų komentaras pridėtas.",
+    
+    "writes..." =>
+    "rašo...",
+    
+    "Posted on" =>
+    "Įrašyta",
+    
+    "Website is not a valid URL." =>
+    "Neteisingas svetainės adresas.",
+    
+    "Name and Comment fields are required." =>
+    "Vardas ir komentaras privalomi",
+    
+    "This comment has been flagged as spam and has been added to the moderation queue." =>
+    "Šis kometras pažymėtas kaip brukalas, todėl jis įtrauktas į moderavimo eilę.",
+    
+    "Comment" =>
+    "Komentras",
+    
+    "Name" =>
+    "Vardas",
+    
+    "Website" =>
+    "Svetainė",
+    
+    "required" =>
+    "privaloma",
+    
+    "Add Comment" =>
+    "Pridėti komentarą",
+    
+    "Do not fill out this field." =>
+    "Nepildykite šio laukelio.",
+    
+    "Only the most recent comments are showing. You need JavaScript to view them all." =>
+    "Tik naujausi komentarai rodomi. Jums reikia naudoti JavaScript, kad peržiūrėtumėt visus.",
+    
+    "Add it!" =>
+    "Pridėti!",
+    
+    "Comments" =>
+    "Komentarai",
+    
+    "Log In" =>
+    "Prisijungti",
+    
+    "Password" =>
+    "Slaptažodis",
+    
+    "All Ham" =>
+    "Visas hamas",
+    
+    "All Spam" =>
+    "Visi brukalai",
+    
+    "Recent Ham" =>
+    "Naujausi hamai",
+    
+    "Recent Spam" =>
+    "Naujausi brukalai",
+    
+    "Delete All Spam" =>
+    "Pašalinti visus brukalus",
+    
+    "Spam" =>
+    "Brukalas",
+    
+    "Ham" =>
+    "Ham",
+    
+    "Recent" =>
+    "Naujausi",
+    
+    "Logout" =>
+    "Atsijungti",
+    
+    "View Guestbook" =>
+    "Peržiūrėti svečių knygą",
+    
+    "Dashboard" =>
+    "Darbastalis",
+    
+    "The comment has been deleted." =>
+    "Komantaras pašalintas",
+    
+    "All spam comments have been deleted." =>
+    "Visi brukalai pašalinti",
+    
+    "No comments to load." =>
+    "Nėra komentarų",
+    
+    "Incorrect password." =>
+    "Neteisingas slaptažodis",
+    
+    "The comment has been reclassified." =>
+    "Kometaras perklasifikuotas",
+    
+    "Not Spam" =>
+    "Ne brukalas",
+    
+    "Delete" =>
+    "Šalinti",
+    
+    "First" =>
+    "Pirmas",
+    
+    "Previous" =>
+    "Buvęs",
+    
+    "Next" =>
+    "Kitas",
+    
+    "Last" =>
+    "Paskutinis"
+);
+?>
